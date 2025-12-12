a12df57e9ae14e8c8a6424522e76669c75da1216
daral <hamedmukhtar@gmail.com>
2025-12-12 13:58:05 +0200

    Create page.tsx

---
+++ frontend-web/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h2>Login</h2>
+      <input placeholder="Email" /><br /><br />
+      <input placeholder="Password" type="password" /><br /><br />
+      <button>Login</button>
+    </main>
+  );
+}
